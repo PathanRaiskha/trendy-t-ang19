d32ec44068b22adb92763c5dfb66143d82060132
PathanRaiskha <pathanrais8407@gmail.com>
2025-07-11 16:42:34 +0530

    chnages in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,17 +18,20 @@ import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dial
 
 import { ProductCardComponent } from './Home/product-card/product-card.component';
 import { HomeComponent } from './Home/home/home.component';
+import { ShowCartListComponent } from './showCartList/show-cart-list/show-cart-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignUpComponent,
-    LoginComponent,
-    MatConfirmDialogComponent,
-    HomeComponent,
-    ProductCardComponent,
-    
-   
+   AppComponent,
+  SignUpComponent,
+  LoginComponent,
+  MatConfirmDialogComponent,
+  HomeComponent,
+  ProductCardComponent,
+  ShowCartListComponent
+  
+
   ],
   imports: [
     BrowserAnimationsModule, // Required for toastr animations
